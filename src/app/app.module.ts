5d5832f93ea6d1d5d26e768a1d9a1c2318f7fb81
Carlos Del Castillo M <maussac13@gmail.com>
2023-05-10 16:40:26 -0500

    hacemos que nuestros formularios sean reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,6 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     LayoutModule,
     NavbarModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
