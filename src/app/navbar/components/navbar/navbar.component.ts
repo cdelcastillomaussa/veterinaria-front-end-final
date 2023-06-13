c6ffe14bb0db681d3a541dfa5c11c0cf7daf97f0
Carlos Del Castillo M <maussac13@gmail.com>
2023-06-12 22:36:24 -0500

    Para hacer que el <mat-sidenav-container> sea responsive y se ajuste automáticamente al ancho del menú lateral cuando el contenido principal se abra o cierre

--- src/app/navbar/components/navbar/navbar.component.ts
+++ src/app/navbar/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
+declare var $: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  [x: string]: any;
+  mobileQuery!: MediaQueryList;
 
-  constructor() { }
+  _mobileQueryListener: () => void;
 
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router, private route: ActivatedRoute) {
+    this.mobileQuery = window.matchMedia('(max-width: 600px)');
+    this.mobileQuery.addEventListener('change', () => {
+      this._mobileQueryListener();
+    });
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+  }
+  
   ngOnInit(): void {
   }
 
